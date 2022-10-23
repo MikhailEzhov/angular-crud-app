fd7b10e9926dd6a5c7e148d5d5efb6e47cd25d01
MikhailEzhov <Hammer.perm@gmail.com>
2022-10-23 13:41:41 +0500

    feat: added component with user table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-crud-app';
+export class AppComponent implements OnInit {
+  title = 'Angular CRUD with Rest API';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
