d358a3eb0a1a6e3d6a227ae4ea520ade71de54ef
MikhailEzhov <Hammer.perm@gmail.com>
2022-10-25 19:42:09 +0500

    feat: added deleting a user from the server

--- src/app/components/user-table-item/user-table-item.component.ts
+++ src/app/components/user-table-item/user-table-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IUser } from 'src/app/models/users.model';
+import { ServerDataChangesService } from 'src/app/services/server-data-changes.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-user-table-item',
@@ -9,7 +11,17 @@ import { IUser } from 'src/app/models/users.model';
 export class UserTableItemComponent implements OnInit {
   @Input() user: IUser;
 
-  constructor() {}
+  constructor(
+    private usersService: UsersService,
+    private serverDataChangesService: ServerDataChangesService
+  ) {}
 
   ngOnInit(): void {}
+
+  deleteUser(id: string): void {
+    this.usersService.deleteUser(id).subscribe((res) => {
+      console.log('DELETE user on server under id:', id);
+      this.serverDataChangesService.changeCount(1);
+    });
+  }
 }
